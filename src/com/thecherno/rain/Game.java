3e8f95cf6f3fa6645a105b6fcd7451f87700b593
mehmet sacli <sacli@outlook.com>
2017-02-10 16:46:35 +0300

    3 bitti

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,12 +1,41 @@
 package com.thecherno.rain;
 
-public class Game {
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
 	
 	public static int width = 300;
 	public static int height = width/16 * 9;
 	public static int scale = 3;
-
 	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	public Game() {
+		
+	}
+	
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		running = false; 
+		try {
+			thread.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		new Game();
+		while (running){
+		}
+	}
 }
 
 
