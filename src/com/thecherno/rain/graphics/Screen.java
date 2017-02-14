6625aa33caaad4efe112b7b2839e1f7ed82094d0
mehmet sacli <sacli@outlook.com>
2017-02-14 15:17:15 +0300

    10 bitti

---
+++ src/com/thecherno/rain/graphics/Screen.java
@@ -0,0 +1,35 @@
+package com.thecherno.rain.graphics;
+
+
+
+public class Screen {
+	private int width, height;
+	public int[] pixels;
+	
+	int time = 0;
+	int counter =0;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width*height];
+	}
+	
+	public void clear() {
+		for(int i =0; i<pixels.length; i++) {
+			pixels[i]=0;
+		}
+	}
+	
+	public void render() {
+		counter++;
+		if(counter%100 ==0) {
+			time++;
+			}
+		for(int y= 0; y < height; y++) {
+			for(int x=0; x<width; x++) {
+				pixels[time+time*width] = 0xff00ff;
+			}
+		}
+	}
+}
